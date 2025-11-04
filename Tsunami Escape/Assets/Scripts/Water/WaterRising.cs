dbad48de6466fb57cd5ff76b661f3efc856ac944
BlueFireKnightYT <vanmeerveldsGames@outlook.com>
2025-11-04 21:45:48 +0100

    De grote heal

--- Tsunami Escape/Assets/Scripts/Water/WaterRising.cs
+++ Tsunami Escape/Assets/Scripts/Water/WaterRising.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 public class WaterRising : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed;
+    Animator animator;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 1f);
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, speed);
     }
 }
