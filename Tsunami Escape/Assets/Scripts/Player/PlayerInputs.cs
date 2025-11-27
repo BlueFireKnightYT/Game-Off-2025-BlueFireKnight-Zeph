bcfb7fdd10ad2b5d1d46c9ebaae88f58ffec818f
Zeph <55712226+ZephMK2@users.noreply.github.com>
2025-11-27 21:51:05 +0100

    miauw

--- Tsunami Escape/Assets/Scripts/Player/PlayerInputs.cs
+++ Tsunami Escape/Assets/Scripts/Player/PlayerInputs.cs
@@ -20,6 +20,8 @@ public class PlayerInputs : MonoBehaviour
     private PlayerCollision playerCollision;
     private float horizontal;
     private ArenaHandler arenaHandler;
+    public bool DoubleJump;
+    private bool HasDoubleJumped;
 
     private void Start()
     {
@@ -74,11 +76,17 @@ public class PlayerInputs : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && IsGrounded())
+        if (context.performed && IsGrounded() == true)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpH);
             animator.SetBool("isWalking", false);
             animator.SetBool("Jumping", true);
+            HasDoubleJumped = false;
+        }
+        else if (context.performed && DoubleJump == true && HasDoubleJumped == false)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpH);
+            HasDoubleJumped = true;
         }
     }
     private bool IsGrounded()
@@ -98,6 +106,7 @@ public class PlayerInputs : MonoBehaviour
             rb.gravityScale = rb.gravityScale - 2;
         }
     }
+ 
 
     public void PauseMenu(InputAction.CallbackContext context)
     {
