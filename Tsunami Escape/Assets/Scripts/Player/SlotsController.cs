c3ceea3b6b5a7d4e71ce1767e34da26704584302
Zeph <55712226+ZephMK2@users.noreply.github.com>
2025-11-30 22:55:54 +0100

    All known relevant bugs fixed, ready for build

--- Tsunami Escape/Assets/Scripts/Player/SlotsController.cs
+++ Tsunami Escape/Assets/Scripts/Player/SlotsController.cs
@@ -66,35 +66,33 @@ public class SlotsController : MonoBehaviour
     {
         do
         {
-            X = Random.Range(1, 7);
+            X = Random.Range(1, 6);
         } while (X == lastX || X == lastY || X == lastZ);   
         if (X == 1) Instantiate(BoostOne, PositionOne);
         if (X == 2) Instantiate(BoostTwo, PositionOne);
         if (X == 3) Instantiate(BoostThree, PositionOne);
         if (X == 4) Instantiate(BoostFour, PositionOne);
         if (X == 5) Instantiate(BoostFive, PositionOne);
-        if (X == 6) Instantiate(BoostSix, PositionOne);
     }
 
     public void UpgradeTwo()
     { 
         do
         {
-            Y = Random.Range(1, 7);
+            Y = Random.Range(1, 6);
         } while (X == Y || Y == lastY || Y == lastX || Y == lastZ);
         if (Y == 1) Instantiate(BoostOne, PositionTwo);
         if (Y == 2) Instantiate(BoostTwo, PositionTwo);
         if (Y == 3) Instantiate(BoostThree, PositionTwo);
         if (Y == 4) Instantiate(BoostFour, PositionTwo);
         if (Y == 5) Instantiate(BoostFive, PositionTwo);
-        if (Y == 6) Instantiate(BoostSix, PositionTwo);
     }
 
     public void UpgradeThree()
     {
         do
         {
-            Z = Random.Range(1, 7);
+            Z = Random.Range(1, 6);
         }
         while (X == Z || Y == Z || Z == lastZ || Z == lastX || Z == lastY);
         if (Z == 1) Instantiate(BoostOne, PositionThree);
@@ -102,7 +100,6 @@ public class SlotsController : MonoBehaviour
         if (Z == 3) Instantiate(BoostThree, PositionThree);
         if (Z == 4) Instantiate(BoostFour, PositionThree);
         if (Z == 5) Instantiate(BoostFive, PositionThree);
-        if (Z == 6) Instantiate(BoostSix, PositionThree);
     }
 
     public void ClickUpgradeDoubleJump()
@@ -118,17 +115,6 @@ public class SlotsController : MonoBehaviour
         if (Z == 1) lastZ = 1;
     }
 
-    public void ClickUpgradeGlider()
-    {
-        Time.timeScale = 1;
-        LootUI = GameObject.Find("ArenaLootMenu");
-        Debug.Log("SlotsController Awake running on: " + gameObject.name + ", LootUI = " + LootUI, this);
-        LootUI.SetActive(false);
-        if (X == 2) lastX = 2;
-        if (Y == 2) lastY = 2;
-        if (Z == 2) lastZ = 2;
-    }
-
     public void ClickUpgradeExtraLife()
     {
         Time.timeScale = 1;
@@ -136,9 +122,9 @@ public class SlotsController : MonoBehaviour
         LootUI = GameObject.Find("ArenaLootMenu");
         LootUI.SetActive(false);
         PC.HasExtraLife = true;
-        if (X == 3) lastX = 3;
-        if (Y == 3) lastY = 3;
-        if (Z == 3) lastZ = 3;
+        if (X == 2) lastX = 2;
+        if (Y == 2) lastY = 2;
+        if (Z == 2) lastZ = 2;
     }
 
     public void ClickUpgradePotionStrength()
@@ -150,9 +136,9 @@ public class SlotsController : MonoBehaviour
         PC.SlowTimeAmount = 4;
         PC.AntiGravity = 2;
         LootUI.SetActive(false);
-        if (X == 4) lastX = 4;
-        if (Y == 4) lastY = 4;
-        if (Z == 4) lastZ = 4;
+        if (X == 3) lastX = 3;
+        if (Y == 3) lastY = 3;
+        if (Z == 3) lastZ = 3;
 
     }
 
@@ -163,9 +149,9 @@ public class SlotsController : MonoBehaviour
         PC = GameObject.Find("Player").GetComponent<PlayerCollision>();
         PC.SurfDuration = 4;
         LootUI.SetActive(false);
-        if (X == 5) lastX = 5;
-        if (Y == 5) lastY = 5;
-        if (Z == 5) lastZ = 5;
+        if (X == 4) lastX = 4;
+        if (Y == 4) lastY = 4;
+        if (Z == 4) lastZ = 4;
     }
 
     public void ClickUpgradeCoinsPlus()
@@ -176,8 +162,8 @@ public class SlotsController : MonoBehaviour
         Debug.Log("SlotsController Awake running on: " + gameObject.name + ", LootUI = " + LootUI, this);
         PC.BaseCoinValue = 5;
         LootUI.SetActive(false);
-        if (X == 6) lastX = 6;
-        if (Y == 6) lastY = 6;
-        if (Z == 6) lastZ = 6;
+        if (X == 5) lastX = 5;
+        if (Y == 5) lastY = 5;
+        if (Z == 5) lastZ = 5;
     }
 }
